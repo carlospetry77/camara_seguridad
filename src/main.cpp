f772eda83b9146501a1ca08fceb8ef2856042345
arkhocpetry <cpetry@arkho.io>
2025-03-03 12:13:46 -0300

    Feature:Add pushBtn simple routine to test inputs and outputs

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,25 @@
 #include <Arduino.h>
+
+//Input Output definitions
+#define pushBtn 16
+#define led 4
+
+//Setup function
 void setup() {
   Serial.begin(115200);
-
+  pinMode(pushBtn, INPUT_PULLUP);
+  pinMode(led,OUTPUT);
 }
 
+
+//Main function
 void loop() {
-  Serial.println("hola");
-  delay(1000);
+  int btnState = digitalRead(pushBtn);
+  if (btnState == LOW) {
+    Serial.println("Se ha pulsado el boton...");
+    digitalWrite(led,HIGH);
+  }else{
+    digitalWrite(led,LOW);
+  }
+  delay(100);
 }
