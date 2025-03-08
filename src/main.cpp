a40bf4ab48ec5d97b394b6bab0e6c3d8ffc960f7
arkhocpetry <cpetry@arkho.io>
2025-03-08 15:09:20 -0300

    Fix: Power problems. Add a cap on VCC

--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,28 @@
 
 //Input Output definitions
 #define pushBtn 16
-#define led 4
 
 //Camera pins
-#define PWDN_GPIO_NUM    -1
-#define RESET_GPIO_NUM   -1
-#define XCLK_GPIO_NUM    0
-#define SIOD_GPIO_NUM    26
-#define SIOC_GPIO_NUM    27
+#define PWDN_GPIO_NUM  32
+#define RESET_GPIO_NUM -1
+#define XCLK_GPIO_NUM  0
+#define SIOD_GPIO_NUM  26
+#define SIOC_GPIO_NUM  27
 
-#define Y9_GPIO_NUM      35
-#define Y8_GPIO_NUM      34
-#define Y7_GPIO_NUM      39
-#define Y6_GPIO_NUM      36
-#define Y5_GPIO_NUM      21
-#define Y4_GPIO_NUM      19
-#define Y3_GPIO_NUM      18
-#define Y2_GPIO_NUM      5
-#define VSYNC_GPIO_NUM   25
-#define HREF_GPIO_NUM    23
-#define PCLK_GPIO_NUM    22
+#define Y9_GPIO_NUM    35
+#define Y8_GPIO_NUM    34
+#define Y7_GPIO_NUM    39
+#define Y6_GPIO_NUM    36
+#define Y5_GPIO_NUM    21
+#define Y4_GPIO_NUM    19
+#define Y3_GPIO_NUM    18
+#define Y2_GPIO_NUM    5
+#define VSYNC_GPIO_NUM 25
+#define HREF_GPIO_NUM  23
+#define PCLK_GPIO_NUM  22
+
+// 4 for flash led or 33 for normal led
+#define LED_GPIO_NUM   4
 
 // 🌐 WiFi credentials
 const char* ssid = WIFI_SSID;
@@ -36,9 +38,7 @@ const char* password = WIFI_PASSWORD;
 void setup() {
   Serial.begin(115200);
   pinMode(pushBtn, INPUT_PULLUP);
-  pinMode(led,OUTPUT);
   Serial.println("📸 Iniciando cámara...");
-  
   //Config camera
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
@@ -60,36 +60,40 @@ void setup() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG;
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG;  // for streaming
+  //config.pixel_format = PIXFORMAT_RGB565; // for face detection/recognition
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
 
-  // IF PSRAM
-  if(psramFound()) {
-      config.frame_size = FRAMESIZE_UXGA;  // 1600x1200
+  // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
+  //                      for larger pre-allocated frame buffer.
+  if (config.pixel_format == PIXFORMAT_JPEG) {
+    if (psramFound()) {
       config.jpeg_quality = 10;
       config.fb_count = 2;
-  } else {
-      config.frame_size = FRAMESIZE_SVGA;  // 800x600
-      config.jpeg_quality = 12;
-      config.fb_count = 1;
-  }
-
-  // Camera Init
-  esp_err_t err = esp_camera_init(&config);
-  if (err != ESP_OK) {
-      Serial.println("❌ Error al iniciar la cámara");
-      return;
-  }
-  Serial.println("✅ Cámara iniciada con éxito");
-
-  // WiFi conn
-    Serial.print("Conectando a WiFi...");
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      // Limit the frame size when PSRAM is not available
+      config.frame_size = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
     }
-    Serial.println("\n✅ WiFi conectado");
-
+  } else {
+    // Best option for face detection/recognition
+    config.frame_size = FRAMESIZE_240X240;
+  }
+  
+  delay(1000);
+  // WiFi conn
+  Serial.print("Conectando a WiFi...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+  }
+  Serial.println("\n✅ WiFi conectado");
 
 }
 
@@ -99,9 +103,7 @@ void loop() {
   int btnState = digitalRead(pushBtn);
   if (btnState == LOW) {
     Serial.println("Se ha pulsado el boton...");
-    digitalWrite(led,HIGH);
   }else{
-    digitalWrite(led,LOW);
   }
   delay(100);
 }
